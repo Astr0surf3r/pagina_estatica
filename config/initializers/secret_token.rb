1224ec31c5f5ac8dba2fb3758c30e6f16262d99d
Astr0surf3r <astr0surf3r@gmail.com>
2011-09-23 20:56:48 -0700

    primero git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaginaEstatica::Application.config.secret_token = 'd737ff8de9fe0368c3f220bc646754b715462557df320388a2418743ef5afc6808351cd6a058e46a7be50fe2ac483b9f59c20ae1f8fbcee1822d8e8745c354e9'
